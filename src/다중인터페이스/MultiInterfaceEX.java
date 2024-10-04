038cdc7415fe1ccef2a7e26aef89312ab679d80d
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-04 12:42:58 +0900

    2024.10.04 4교시

--- src/다중인터페이스/MultiInterfaceEX.java
+++ src/다중인터페이스/MultiInterfaceEX.java
@@ -2,6 +2,7 @@ package 다중인터페이스;
 
 public class MultiInterfaceEX {
     public static void main(String[] args) {
-
+        SportCar porsche = new SportCar(true, 20, false, true);
+        porsche.viewInfo();
     }
 }
--- src/다중인터페이스/SportCar.java
+++ src/다중인터페이스/SportCar.java
@@ -17,25 +17,42 @@ public class SportCar extends Car implements AirCon, AutoDrive{
     @Override
     public void airConON() {
         this.isAirCon = true;
+        System.out.println("에어컨을 켰습니다.");
     }
 
     @Override
     public void airConOFF() {
         this.isAirCon = false;
+        System.out.println("에어컨을 껐습니다.");
     }
 
     @Override
     public void setAirConTemp(int tmp) {
-
+        setTemp = tmp;
+        System.out.println("에어컨 설정 온도를 " + tmp + "로 변경 합니다.");
     }
 
     @Override
     public void autoDrvON() {
-
+        isAutoDrv = true;
+        System.out.println("자율 주행 모드를 켰습니다.");
     }
 
     @Override
     public void autoDrvOFF() {
-
+        isAutoDrv = false;
+        System.out.println("자율 주행 모드를 껐습니다.");
+    }
+    public void viewInfo() {
+        String airConStr  = (isAirCon) ? "ON" : "OFF";
+        String autoStr  = (isAutoDrv) ? "ON" : "OFF";
+        String turboStr  = (isTurbo) ? "ON" : "OFF";
+        System.out.println("속도 : " + speed);
+        System.out.println("색상 : " + color);
+        System.out.println("제작연도 : " + year);
+        System.out.println("에어컨 : " + airConStr);
+        System.out.println("에어컨 온도 : " + setTemp);
+        System.out.println("자율 주행 : " + autoStr);
+        System.out.println("터보 모드 : " + turboStr);
     }
 }
